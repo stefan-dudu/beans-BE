c7a0cfaa25df7c697028dad0fab925a440dfe996
Stefan Dudu <stefan01.dev@gmail.com>
2024-02-26 17:41:53 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 5001;
+
+app.get("/", (req, res) => {
+  // res.send("Data beeing sent from BE");
+  res.json({ id: 2, user: "mike" });
+});
+
+// app.post("/", (req, res) => {
+//   res.send("you can post here");
+// });
+
+app.listen(port, () => {
+  console.log(`it ran 2nd time`);
+});
