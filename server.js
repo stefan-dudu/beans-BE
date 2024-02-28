6be297a7ae7a73b02f7780bdc3b6a76b8d32f1fb
Stefan Dudu <stefan01.dev@gmail.com>
2024-02-28 16:50:26 +0200

    CRUD api

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get("/api/v1/beans", (req, res) => {
   });
 });
 
-// console.log("beans", beans[beans.length - 2]._id);
+// console.log("beans", beans[beans.length - 2].id);
 
 app.post("/api/v1/beans", (req, res) => {
-  const newId = beans[beans.length - 1]._id + 1;
+  const newId = beans[beans.length - 1].id + 1;
   const newBean = Object.assign({ id: newId }, req.body);
   beans.push(newBean);
   fs.writeFile(`${__dirname}/data/beans.json`, JSON.stringify(beans), (err) => {
@@ -26,8 +26,55 @@ app.post("/api/v1/beans", (req, res) => {
       data: {
         beans: newBean,
       },
-      // beans: newBean,
     });
+  });
+});
+
+app.get("/api/v1/beans/:id", (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+
+  if (id > beans.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  const bean = beans.find((el) => el.id === id);
+
+  res.status(200).json({
+    status: "success",
+    data: { bean },
+  });
+});
+
+app.patch("/api/v1/beans/:id", (req, res) => {
+  if (req.params.id * 1 > beans.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      bean: "<Updated bean !..",
+    },
+  });
+});
+
+app.delete("/api/v1/beans/:id", (req, res) => {
+  if (req.params.id * 1 > beans.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+  res.status(204).json({
+    status: "success",
+    data: null,
   });
 });
 
