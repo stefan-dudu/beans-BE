62d6ac58003590aed3b5e8b387b0e6019ab2dca3
Stefan Dudu <stefan01.dev@gmail.com>
2024-02-29 17:47:07 +0200

    successfully posting to mongoDB - mongoose v5

--- server.js
+++ server.js
@@ -1,8 +1,48 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
+
+dotenv.config({ path: './.env' });
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connection successful');
+  });
+
+const beanSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A bean must have a price'],
+    unique: true,
+  },
+  rating: { type: Number, default: 3.33 },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Bean = mongoose.model('Bean', beanSchema);
+
+const testBean = new Bean({
+  name: 'Brasil',
+  rating: 3,
+  price: 10,
+});
+
+testBean
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
