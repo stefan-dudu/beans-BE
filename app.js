6194beee62163649b865b622f27e0c8af8a78846
Stefan Dudu <stefan01.dev@gmail.com>
2024-03-12 17:56:15 +0200

    additional sanitization & security mesures

--- app.js
+++ app.js
@@ -2,22 +2,64 @@ const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSantize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const beansRouter = require('./routes/beansRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middlewares
-// console.log(process.env.NODE_ENV);
+//  1) GLOBAL MIDDLEWARES
+
+// set securitiy http headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// limit requests from the same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP. Try again over an hour!',
+});
+app.use('/api', limiter);
+
+// body parser eading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL QUERY INJECTION
+app.use(mongoSantize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Routes
 app.use('/api/v1/beans', beansRouter);
 app.use('/api/v1/users', userRouter);
+
+// prevent prarameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'origin',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'roastLevel',
+      'flavorNotes',
+      'aroma',
+      'acidity',
+      'body',
+      'price',
+    ],
+  }),
+);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
--- controllers/authController.js
+++ controllers/authController.js
@@ -81,6 +81,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+    console.log('token', token);
   }
 
   if (!token) {
