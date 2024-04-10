b9eba4d918dd0f123ef057d1628741d1874090da
Stefan Dudu <stefan01.dev@gmail.com>
2024-04-10 10:42:32 +0300

    8 setCookies

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://fotbalist.net',
+      'https://fotbalist.net/',
       'https://baristretto.com',
     ],
     credentials: true,
