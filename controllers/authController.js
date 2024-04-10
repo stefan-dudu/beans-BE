c240f1b7122e9bcfce44494c870c2de7578e0cb3
Stefan Dudu <stefan01.dev@gmail.com>
2024-04-10 12:27:21 +0300

    15 setCookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,11 +21,11 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
-    domain: '.fotbalist.net',
+    domain: 'fotbalist.net',
     path: '/',
     httpOnly: true,
     secure: true,
-    sameSite: 'None', // Required for cross-site requests
+    sameSite: 'Lax', // Required for cross-site requests
     credentials: 'include',
   };
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
